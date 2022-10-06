0cf1c7daf0e488687f7ffb5a469522ff434659cb
MaksimNosov <m.s.nosov@gmail.com>
2022-10-06 20:36:39 +0300

    Task 1 + Task 2

--- src/test/java/ru/netology/AppCardDeliveryTestTask1.java
+++ src/test/java/ru/netology/AppCardDeliveryTestTask1.java
@@ -1,7 +1,7 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -32,143 +31,130 @@ public class AppCardDeliveryTestTask1 {
     int validDuration = 15;
     String msgFieldNotFilled = "Поле обязательно для заполнения";
     String msgInvalidCity = "Доставка в выбранный город недоступна";
+    String msgInvalidDate = "Неверно введена дата";
     String msgInvalidName = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
     String msgInvalidPhone = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-    String msgSuccess = "Успешно";
-    String msgSuccessDate = "Встреча успешно забронирована на";
+    String msgSuccess = "Успешно! Встреча успешно забронирована на " + validDate;
 
+    @BeforeEach
+    void host() {
+        open("http://localhost:9999/");
+    }
 
     @Test
     void positiveTest() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
-        $(withText(msgSuccess)).shouldBe(visible, Duration.ofSeconds(validDuration));
-        $("[data-test-id='notification']").shouldHave(Condition.text(msgSuccessDate));
-        $("[data-test-id='notification']").shouldHave(Condition.text(validDate));
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(validDuration));
+        $("[data-test-id='notification']").shouldHave(Condition.text(msgSuccess));
     }
 
     @Test
     void noCity() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        host();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
         $("[data-test-id='city'] .input__sub").shouldHave(Condition.exactText(msgFieldNotFilled));
     }
 
     @Test
     void inValidCity() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(invalidCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(invalidCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
         $("[data-test-id='city'] .input__sub").shouldHave(Condition.exactText(msgInvalidCity));
     }
 
     @Test
     void noDate() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
+        $("[data-test-id='date'] .input__sub").shouldHave(Condition.exactText(msgInvalidDate));
     }
 
     @Test
     void inValidDate() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(invalidDate);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(invalidDate);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
+        $("[data-test-id='date'] .input__sub").shouldHave(Condition.exactText(msgInvalidDate));
     }
 
     @Test
     void noName() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
         $("[data-test-id='name'] .input__sub").shouldHave(Condition.exactText(msgFieldNotFilled));
     }
 
     @Test
     void invalidName() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='name'] input").setValue(invalidName);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='name'] input").setValue(invalidName);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
         $("[data-test-id='name'] .input__sub").shouldHave(Condition.exactText(msgInvalidName));
     }
 
     @Test
     void noPhone() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
         $("[data-test-id='phone'] .input__sub").shouldHave(Condition.exactText(msgFieldNotFilled));
     }
 
     @Test
     void invalidPhone() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='phone'] input").setValue(invalidPhone);
-        form.$("[data-test-id='agreement']").click();
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='phone'] input").setValue(invalidPhone);
+        $("[data-test-id='agreement']").click();
+        $(".button_view_extra").click();
         $("[data-test-id='phone'] .input__sub").shouldHave(Condition.exactText(msgInvalidPhone));
     }
 
     @Test
     void noAgreement() {
-        open("http://localhost:9999/");
-        SelenideElement form = $("#root");
-        form.$("[data-test-id='city'] input").setValue(validCity);
-        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        form.$("[data-test-id='date'] input").setValue(validDate);
-        form.$("[data-test-id='name'] input").setValue(validName);
-        form.$("[data-test-id='phone'] input").setValue(validPhone);
-        form.$(".button_view_extra").click();
+        $("[data-test-id='city'] input").setValue(validCity);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(validDate);
+        $("[data-test-id='name'] input").setValue(validName);
+        $("[data-test-id='phone'] input").setValue(validPhone);
+        $(".button_view_extra").click();
         $("[data-test-id='agreement'].input_invalid").shouldBe(visible);
     }
 }
--- src/test/java/ru/netology/AppCardDeliveryTestTask2.java
+++ src/test/java/ru/netology/AppCardDeliveryTestTask2.java
@@ -1,19 +1,13 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
-import org.openqa.selenium.Keys;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,80 +16,27 @@ public class AppCardDeliveryTestTask2 {
     String validCity = "Казань";
     String firstTwoLettersOfCity = validCity.substring(0, 2);
     int dateDeliveryMoreCurrentDateByDays = 7;
-    String validDate;
-    String unitDateInDate = "27.10.2022";
-    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-    Date unitDate = formatter.parse(unitDateInDate);
     String validName = "Составная-Фамилия Имя";
     String validPhone = "+79998887766";
     int validDuration = 15;
-    String msgSuccess = "Успешно";
-    String msgSuccessDate = "Встреча успешно забронирована на";
-
-    public AppCardDeliveryTestTask2() throws ParseException {
-    }
-
-    String generateDate(int days, String dateFormat) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(dateFormat));
-    }
-
-    String validDateRequirements = generateDate(7, "dd.MM.yyyy");
+    String msgSuccess = "Успешно! Встреча успешно забронирована на " + LocalDate.now().plusDays(dateDeliveryMoreCurrentDateByDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     @Test
     void positiveTestUniversalDate() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue(firstTwoLettersOfCity);
         $(".input__popup").find(byText(validCity)).click();
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] .icon").click();
-        int today = Integer.parseInt($(".calendar__day_state_today").getText());
-        int lastDay = Integer.parseInt($$("[data-day]").last().getText());
-        if (today <= lastDay - dateDeliveryMoreCurrentDateByDays) {
-        validDate = LocalDate.now().plusDays(dateDeliveryMoreCurrentDateByDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        } else {
+        if (LocalDate.now().getDayOfMonth() > (LocalDate.MAX.getDayOfMonth() - dateDeliveryMoreCurrentDateByDays)) {
             $("[data-step='1']").click();
-            int difference = Integer.parseInt($$("[data-day]").last().getText()) - today + dateDeliveryMoreCurrentDateByDays;
-        validDate = Integer.toString(Integer.parseInt($("[data-day]").getText()) + difference);
         }
-        $("[data-test-id='date'] input").setValue(validDate);
+        String date = Integer.toString(LocalDate.now().plusDays(dateDeliveryMoreCurrentDateByDays).getDayOfMonth());
+        $(byText(date)).click();
         $("[data-test-id='name'] input").setValue(validName);
         $("[data-test-id='phone'] input").setValue(validPhone);
         $("[data-test-id='agreement']").click();
         $(".button_view_extra").click();
-        $(withText(msgSuccess)).shouldBe(visible, Duration.ofSeconds(validDuration));
-        $("[data-test-id='notification']").shouldHave(Condition.text(msgSuccessDate));
-        $("[data-test-id='notification']").shouldHave(Condition.text(validDate));
-        System.out.println(validDate);
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(validDuration));
+        $("[data-test-id='notification']").shouldHave(Condition.text(msgSuccess));
     }
-
-    @Test
-    void positiveTestUnitDate() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue(firstTwoLettersOfCity);
-        $(".input__popup").find(byText(validCity)).click();
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] .icon").click();
-//        int today = Integer.parseInt(String.valueOf(unitDate));
-        System.out.println(unitDate);
-//        System.out.println(today);
-//        int lastDay = Integer.parseInt($$("[data-day]").last().getText());
-//        if (today <= lastDay - dateDeliveryMoreCurrentDateByDays) {
-////            validDate = LocalDate.now().plusDays(dateDeliveryMoreCurrentDateByDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-//            validDate = unitDate.plusDays(dateDeliveryMoreCurrentDateByDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-//        } else {
-//            $("[data-step='1']").click();
-//            int difference = Integer.parseInt($$("[data-day]").last().getText()) - today + dateDeliveryMoreCurrentDateByDays;
-//            validDate = Integer.toString(Integer.parseInt($("[data-day]").getText()) + difference);
-//        }
-//        $("[data-test-id='date'] input").setValue(validDate);
-//        $("[data-test-id='name'] input").setValue(validName);
-//        $("[data-test-id='phone'] input").setValue(validPhone);
-//        $("[data-test-id='agreement']").click();
-//        $(".button_view_extra").click();
-//        $(withText(msgSuccess)).shouldBe(visible, Duration.ofSeconds(validDuration));
-//        $("[data-test-id='notification']").shouldHave(Condition.text(msgSuccessDate));
-//        $("[data-test-id='notification']").shouldHave(Condition.text(validDate));
-//        System.out.println(validDate);
-    }
-
 }
